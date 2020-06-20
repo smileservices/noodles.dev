3b5a0e46d2ee4c335a4fdae3aa3c4d9fc3505e6f
vladimir.gorea <vladimir.gorea@gmail.com>
2020-06-20 10:30:49 +0700

    Implement autotranslate with google API key/service_account type of auth

---
+++ app/autotranslate/management/commands/translate_messages.py
@@ -0,0 +1,204 @@
+import logging
+import os
+import re
+from optparse import make_option
+
+import polib
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from autotranslate.utils import get_translator
+
+logger = logging.getLogger(__name__)
+
+# not sure whether we actually need this
+# just making this change for backward compatibility
+# it was always empty anyways
+# https://github.com/django/django/blob/1.9/django/core/management/base.py#L210
+default_options = () if not hasattr(BaseCommand, 'option_list') \
+    else BaseCommand.option_list
+
+
+class Command(BaseCommand):
+    help = ('autotranslate all the message files that have been generated '
+            'using the `makemessages` command.')
+
+    option_list = default_options + (
+        make_option('--locale', '-l', default=[], dest='locale', action='append',
+                    help='autotranslate the message files for the given locale(s) (e.g. pt_BR). '
+                         'can be used multiple times.'),
+        make_option('--untranslated', '-u', default=False, dest='skip_translated', action='store_true',
+                    help='autotranslate the fuzzy and empty messages only.'),
+        make_option('--set-fuzzy', '-f', default=False, dest='set_fuzzy', action='store_true',
+                    help='set the fuzzy flag on autotranslated messages.'),
+    )
+
+    def add_arguments(self, parser):
+        # Previously, only the standard optparse library was supported and
+        # you would have to extend the command option_list variable with optparse.make_option().
+        # See: https://docs.djangoproject.com/en/1.8/howto/custom-management-commands/#accepting-optional-arguments
+        # In django 1.8, these custom options can be added in the add_arguments()
+        parser.add_argument('--locale', '-l', default=[], dest='locale', action='append',
+                            help='autotranslate the message files for the given locale(s) (e.g. pt_BR). '
+                                 'can be used multiple times.')
+        parser.add_argument('--untranslated', '-u', default=False, dest='skip_translated', action='store_true',
+                            help='autotranslate the fuzzy and empty messages only.')
+        parser.add_argument('--set-fuzzy', '-f', default=False, dest='set_fuzzy', action='store_true',
+                            help='set the fuzzy flag on autotranslated messages.')
+
+    def set_options(self, **options):
+        self.locale = options['locale']
+        self.skip_translated = options['skip_translated']
+        self.set_fuzzy = options['set_fuzzy']
+
+    def handle(self, *args, **options):
+        self.set_options(**options)
+
+        assert getattr(settings, 'USE_I18N', False), 'i18n framework is disabled'
+        assert getattr(settings, 'LOCALE_PATHS', []), 'locale paths is not configured properly'
+        for directory in settings.LOCALE_PATHS:
+            # walk through all the paths
+            # and find all the pot files
+            for root, dirs, files in os.walk(directory):
+                for file in files:
+                    if not file.endswith('.po'):
+                        # process file only
+                        # if its a pot file
+                        continue
+
+                    # get the target language from the parent folder name
+                    target_language = os.path.basename(os.path.dirname(root))
+
+                    if self.locale and target_language not in self.locale:
+                        logger.info('skipping translation for locale `{}`'.format(target_language))
+                        continue
+
+                    self.translate_file(root, file, target_language)
+
+    def translate_file(self, root, file_name, target_language):
+        """
+        convenience method for translating a pot file
+
+        :param root:            the absolute path of folder where the file is present
+        :param file_name:       name of the file to be translated (it should be a pot file)
+        :param target_language: language in which the file needs to be translated
+        """
+        logger.info('filling up translations for locale `{}`'.format(target_language))
+
+        po = polib.pofile(os.path.join(root, file_name))
+        strings = self.get_strings_to_translate(po)
+
+        # translate the strings,
+        # all the translated strings are returned
+        # in the same order on the same index
+        # viz. [a, b] -> [trans_a, trans_b]
+        tl = get_translator()
+        translated_strings = tl.translate_strings(strings, target_language, 'en', False)
+        self.update_translations(po, translated_strings)
+        po.save()
+
+    def need_translate(self, entry):
+        return not entry.obsolete and (not (self.skip_translated and entry.translated()))
+
+    def get_strings_to_translate(self, po):
+        """Return list of string to translate from po file.
+
+        :param po: POFile object to translate
+        :type po: polib.POFile
+        :return: list of string to translate
+        :rtype: collections.Iterable[six.text_type]
+        """
+        strings = []
+        for index, entry in enumerate(po):
+            if not self.need_translate(entry):
+                continue
+            strings.append(humanize_placeholders(entry.msgid))
+            if entry.msgid_plural:
+                strings.append(humanize_placeholders(entry.msgid_plural))
+        return strings
+
+    def update_translations(self, entries, translated_strings):
+        """Update translations in entries.
+
+        The order and number of translations should match to get_strings_to_translate() result.
+
+        :param entries: list of entries to translate
+        :type entries: collections.Iterable[polib.POEntry] | polib.POFile
+        :param translated_strings: list of translations
+        :type translated_strings: collections.Iterable[six.text_type]
+        """
+        translations = iter(translated_strings)
+        for entry in entries:
+            if not self.need_translate(entry):
+                continue
+
+            if entry.msgid_plural:
+                try:
+                    # fill the first plural form with the entry.msgid translation
+                    translation = next(translations)
+                    translation = fix_translation(entry.msgid, translation)
+                    entry.msgstr_plural[0] = translation
+
+                    # fill the rest of plural forms with the entry.msgid_plural translation
+                    translation = next(translations)
+                    translation = fix_translation(entry.msgid_plural, translation)
+                    for k, v in entry.msgstr_plural.items():
+                        if k != 0:
+                            entry.msgstr_plural[k] = translation
+                except StopIteration:
+                    continue
+            else:
+                try:
+                    translation = next(translations)
+                    translation = fix_translation(entry.msgid, translation)
+                    entry.msgstr = translation
+                except StopIteration:
+                    continue
+
+            # Set the 'fuzzy' flag on translation
+            if self.set_fuzzy and 'fuzzy' not in entry.flags:
+                entry.flags.append('fuzzy')
+
+
+def humanize_placeholders(msgid):
+    """Convert placeholders to the (google translate) service friendly form.
+
+    %(name)s -> __name__
+    %s       -> __item__
+    %d       -> __number__
+    """
+    return re.sub(
+        r'%(?:\((\w+)\))?([sd])',
+        lambda match: r'__{0}__'.format(
+            match.group(1).lower() if match.group(1) else 'number' if match.group(2) == 'd' else 'item'),
+        msgid)
+
+
+def restore_placeholders(msgid, translation):
+    """Restore placeholders in the translated message."""
+    placehoders = re.findall(r'(%(?:\(\w+\))?[sd])', msgid)
+    try:
+        restored = re.sub(
+            r'(__[\w]+?__)',
+            lambda matches: f'{placehoders.pop()}',
+            translation)
+    except IndexError:
+        # sometimes there appears more placeholders inside the translated text...
+        logger.error(f'Error while restoring placeholders to \n{msgid} \nfrom \n{translation}.\nPlaceholders are:\n{placehoders}')
+        restored=""
+    return restored
+
+
+def fix_translation(msgid, translation):
+    # Google Translate removes a lot of formatting, these are the fixes:
+    # - Add newline in the beginning if msgid also has that
+    if msgid.startswith('\n') and not translation.startswith('\n'):
+        translation = u'\n' + translation
+
+    # - Add newline at the end if msgid also has that
+    if msgid.endswith('\n') and not translation.endswith('\n'):
+        translation += u'\n'
+
+    # Remove spaces that have been placed between %(id) tags
+    translation = restore_placeholders(msgid, translation)
+    return translation
---
+++ app/autotranslate/services.py
@@ -0,0 +1,172 @@
+import collections, six, json, requests
+import logging
+from django.conf import settings
+from google.cloud import translate_v2 as translate
+
+logger = logging.getLogger('autotranslate')
+
+
+class BaseTranslatorService:
+    """
+    Defines the base methods that should be implemented
+    """
+
+    def translate_string(self, text, target_language, source_language='en'):
+        """
+        Returns a single translated string literal for the target language.
+        """
+        raise NotImplementedError('.translate_string() must be overridden.')
+
+    def translate_strings(self, strings, target_language, source_language='en', optimized=True):
+        """
+        Returns a iterator containing translated strings for the target language
+        in the same order as in the strings.
+        :return:    if `optimized` is True returns a generator else an array
+        """
+        raise NotImplementedError('.translate_strings() must be overridden.')
+
+
+class GoogleAPITranslatorService(BaseTranslatorService):
+    """
+    Uses the paid Google API for translating.
+    https://github.com/google/google-api-python-client
+    """
+
+    def __init__(self, max_segments=128):
+        self.translate_key = getattr(settings, 'GOOGLE_TRANSLATE_KEY', False)
+        self.service_account_key = getattr(settings, 'GOOGLE_APPLICATION_CREDENTIALS', False)
+        if not self.service_account_key:
+            assert self.translate_key, 'To use GoogleAPI translation you need to add GOOGLE_TRANSLATE_KEY or GOOGLE_APPLICATION_CREDENTIALS in settings.py'
+            self.request_type = 'api_key_request'
+        else:
+            self.request_type = 'service_account_key_request'
+        logger.info(f'Using {self.request_type} ...')
+        # the google translation API has a limit of max
+        # 128 translations in a single request
+        # and throws `Too many text segments Error`
+        self.max_segments = max_segments
+        self.translated_strings = []
+
+    def map_language_code(self, code):
+        codes = {
+            'vn': 'vi'
+        }
+        if code in codes:
+            return codes[code]
+        else:
+            return code
+
+    def api_key_request(self, text, source, target):
+        url = f'https://translation.googleapis.com/language/translate/v2?key={self.translate_key}'
+        data = {
+            'source': source,
+            'target': target,
+            'q': text
+        }
+        res = requests.post(url, data)
+        logger.info(f'Posted to {url} data: {data} --- status {res.status_code}')
+        if res.status_code == 200:
+            return res
+        else:
+            logger.error(f'Got error from google api {res.status_code} with response {res.text}')
+            raise Exception('Received a bad status code from google api')
+
+    def service_account_key_request(self, text, source, target):
+        service = translate.Client()
+        translations = service.translate(text, target_language=target, source_language=source)
+        return [t['translatedText'] for t in translations]
+
+    def send_request(self, text, source, target):
+        target = self.map_language_code(target)
+        if self.request_type == 'api_key_request':
+            response = self.api_key_request(text, source, target)
+            return self.get_list_from_response(response)
+        if self.request_type == 'service_account_key_request':
+            return self.service_account_key_request(text, source, target)
+
+    def translate_string(self, text, target_language, source_language='en'):
+        assert isinstance(text, six.string_types), '`text` should a string literal'
+        response_arr = self.send_request(text=text, source=source_language, target=target_language)
+        return response_arr[0]
+
+    def get_list_from_response(self, response):
+        return [t['translatedText'] for t in json.loads(response.text)['data']['translations']]
+
+    def translate_strings(self, strings, target_language, source_language='en', optimized=True):
+        assert isinstance(strings, collections.MutableSequence), \
+            '`strings` should be a sequence containing string_types'
+        if len(strings) == 0:
+            return []
+        elif len(strings) <= self.max_segments:
+            setattr(self, 'translated_strings', getattr(self, 'translated_strings', []))
+            translations = self.send_request(text=strings, source=source_language, target=target_language)
+            self.translated_strings.extend(translations)
+            return self.translated_strings
+        else:
+            self.translate_strings(strings[0:self.max_segments], target_language=target_language,
+                                   source_language=source_language)
+            _translated_strings = self.translate_strings(strings[self.max_segments:], target_language, source_language)
+
+            # reset the property or it will grow with subsequent calls
+            self.translated_strings = []
+            return _translated_strings
+
+
+class AzureAPITranslatorService(BaseTranslatorService):
+    """
+    Use Azure Translator Text
+    https://docs.microsoft.com/en-us/azure/cognitive-services/translator/reference/v3-0-reference
+    """
+
+    def __init__(self, max_segments=256):
+        self.azure_translator_secret_key = getattr(settings, 'AZURE_TRANSLATOR_SECRET_KEY', None)
+        assert self.azure_translator_secret_key, (
+            '`AZURE_TRANSLATOR_SECRET_KEY` is not configured, it is required by `Azure Translator`')
+
+        self.max_segments = max_segments
+
+    def azure_translate(self, strings, target_language, source_language='en'):
+        from urllib.parse import quote
+        import requests, re
+
+        translated_strings = []
+        headers = {'Ocp-Apim-Subscription-Key': self.azure_translator_secret_key,
+                   'Content-type': 'application/json'}
+        for string in strings:
+            base_url = 'https://api.cognitive.microsofttranslator.com/'
+            path = 'translate?api-version=3.0&'
+            params = 'from={}&to={}'.format(source_language, target_language)
+            url = '{}{}{}'.format(base_url, path, params)
+            data = [{'Text': string}]
+            response = requests.post(url, headers=headers, json=data)
+            response = response.json()
+            if response[0]['translations']:
+                translated_strings.append(response[0]['translations'][0]['text'])
+                logger.info('Translate "{}" to "{}"'.format(string, response[0]['translations'][0]['text']))
+            else:
+                logger.info('Didn\'t get translate for "{}" from Azure.'.format(string))
+        return translated_strings
+
+    def translate_string(self, text, target_language, source_language='en'):
+        assert isinstance(text, six.string_types), '`text` should a string literal'
+        response = self.azure_translate(strings=[text], target=target_language, source=source_language)
+        return response
+
+    def translate_strings(self, strings, target_language, source_language='en', optimized=True):
+        assert isinstance(strings,
+                          collections.MutableSequence), '`strings` should be a sequence containing string_types'
+
+        if len(strings) == 0:
+            logger.info('There are no strings request to translate for "{}".'.format(target_language))
+            return []
+        elif len(strings) <= self.max_segments:
+            setattr(self, 'translated_strings', getattr(self, 'translated_strings', []))
+            response = self.azure_translate(strings=strings, target_language=target_language,
+                                            source_language=source_language)
+            if response:
+                return response
+            else:
+                return []
+        else:
+            # reset the property or it will grow with subsequent calls
+            return []
