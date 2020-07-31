b73f7d53a37a0abf76d51e169f5ea9d66f703c25
vladimir.gorea <vladimir.gorea@gmail.com>
2020-07-31 19:17:15 +0700

    Clean up old code and add show admin navbar in frontend section if user is logged in

--- app/dashboard/src/layout/ContentLayout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ContentLayout(props) {
-    return (
-        <div id="content">
-            <props.content />
-        </div>
-    )
-}
--- app/frontend/src/layout/ContentLayout.js
+++
@@ -1,9 +0,0 @@
-import React, {useState} from "react";
-
-export default function ContentLayout({content}) {
-    return (
-        <main role="main" className="container">
-            {content}
-        </main>
-    )
-}
