ca7b0ed852c53d9973eb79478a26ef884df13304
vladimir.gorea <vladimir.gorea@gmail.com>
2020-09-01 22:02:01 +0700

    Add static pages for terms and add footer

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
+from django.shortcuts import render
 from frontend import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('terms/privacy-policy', lambda request: render(request, 'frontend/static/privacy.html'), name='privacy-policy'),
+    path('terms/terms-conditions', lambda request: render(request, 'frontend/static/terms.html'), name='terms-policy'),
+    path('terms/cookies-policy', lambda request: render(request, 'frontend/static/cookie-terms.html'), name='cookie-policy'),
 ]
