1a3521ce53089ffe5de8d863e2f388f0a3622f1a
vladimir.gorea <vladimir.gorea@gmail.com>
2020-09-02 13:16:30 +0700

    Pack slimselect.scss to main.scss to reduce requests; use react.min in react bundle

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,13 @@ module.exports = {
     },
     entry: {
         // just create the react-bundle once
-        // 'static/js/react-bundle': ['./app/static/js/react.min.js', './app/static/js/react-dom.min.js'],
+
+        //for dev
         // 'static/js/react-bundle': ['./app/static/js/react.development.js', './app/static/js/react-dom.development.js'],
+
+        //for prod
+        // 'static/js/react-bundle': ['./app/static/js/react.min.js', './app/static/js/react-dom.min.js'],
+
         '/static/study_resource/study-resource-detail-reviews': './app/study_resource/src/StudyResourceDetailReviews.js',
         '/static/study_resource/study-resource-create': './app/study_resource/src/create/StudyResourceCreate.js',
         '/static/study_resource/study-resource-edit': './app/study_resource/src/edit/StudyResourceEdit.js',
@@ -14,7 +19,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: __dirname +'/app/'
+        path: __dirname + '/app/'
     },
     module: {
         rules: [
