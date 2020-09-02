3278a279791f2bbe51b15a5c9a9702d65ac382cb
vladimir.gorea <vladimir.gorea@gmail.com>
2020-09-02 12:45:55 +0700

    Add sentry to prod settings and change a little text on homepage

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -1,5 +1,18 @@
 from . import *
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://d21d5192b99349409121a487136c4178@o424360.ingest.sentry.io/5414175",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 if DEBUG:
     raise ValueError('Running production settings with DEBUG set to True')
 
