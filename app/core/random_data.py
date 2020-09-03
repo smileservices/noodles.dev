0775311cbace38ef54001d38778c0692ee35f936
vladimir.gorea <vladimir.gorea@gmail.com>
2020-09-03 18:32:16 +0700

    Increase randomness of user email for fake users

--- app/core/random_data.py
+++ app/core/random_data.py
@@ -7,6 +7,6 @@ EMAIL_PROVIDERS = [
 
 def email(firstName, lastName, provider=False):
     separator = random.choice(['_', '', '-', '.'])
-    firstpart = f'{firstName}{separator}{lastName}'.lower()
+    firstpart = f'{firstName}{separator}{lastName}-{random.randint(100,9999)}'.lower()
     email_provider = provider if provider else random.choice(EMAIL_PROVIDERS)
     return f'{firstpart}@{email_provider}'
