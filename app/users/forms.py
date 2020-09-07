170a9d88b03091dcccaf2feb7dee0f98a840b6d2
vladimir.gorea <vladimir.gorea@gmail.com>
2020-09-07 11:34:30 +0700

    Add recaptcha with django-recaptcha

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,10 +1,21 @@
 from django.forms import Form, CharField, ModelForm
+from allauth.account.forms import SignupForm as AllAuthSignUpForm
 from .models import CustomUser
+from captcha.fields import ReCaptchaField
 
 
-class SignupForm(Form):
+class SignupForm(AllAuthSignUpForm):
     first_name = CharField(max_length=30, label='First Name')
     last_name = CharField(max_length=150, label='Last Name')
+    captcha = ReCaptchaField()
+    field_order = [
+        'first_name',
+        'last_name',
+        'email',
+        'password1',
+        'password2',
+        'captcha'
+    ]
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
