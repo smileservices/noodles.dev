4c6cda1c63387fe6c4c68ba91b64d30ce400e753
vladimir.gorea <vladimir.gorea@gmail.com>
2020-10-03 20:09:00 +0700

    problem_solution - problem detail

--- app/problem_solution/views.py
+++ app/problem_solution/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.decorators import login_required
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -6,7 +7,7 @@ from rest_framework.filters import OrderingFilter, SearchFilter
 from django.urls import reverse_lazy
 from . import filters
 import problem_solution.serializers as serializers
-
+from problem_solution.models import Problem, Solution
 
 # Create your views here.
 
@@ -34,8 +35,18 @@ def problem_create(request, *args, **kwargs):
     return render(request, 'problem_solution/problem/create_page.html')
 
 
-def problem_detail(request, *args, **kwargs):
-    pass
+def problem_detail(request, id, slug):
+    queryset = Problem.objects
+    resource = queryset.get(pk=id)
+    related = queryset.filter(
+        Q(tags__in=resource.tags.all()),
+        ~Q(id=resource.id)
+    )[:5]
+    data = {
+        'result': resource,
+        'related': related,
+    }
+    return render(request, 'problem_solution/problem/detail_page.html', data)
 
 
 def solution_detail(request, *args, **kwargs):
