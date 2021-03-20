331daf24052024c869e524c3dd674e4b9ca2a970
vladimir.gorea <vladimir.gorea@gmail.com>
2021-03-20 13:45:17 +0700

    RelatedApp - fix missing tab arg in addFilterFactory Restyle Footer

--- app/frontend/src/RelatedApp.js
+++ app/frontend/src/RelatedApp.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import RelatedComponent from "../../search/src/RelatedComponent";
 
 function RelatedApp() {
-    function addFilterFactory() {
+    function addFilterFactory(tab) {
         return (name, value) => window.location = '/search/?tab=' + tab + '&' + name + '=' + value;
     }
     return (<RelatedComponent addFilter={addFilterFactory('resources')}/>)
