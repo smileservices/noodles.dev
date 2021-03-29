bbc2f586e19f4d28a275062b9f8043a6422e9e41
vladimir.gorea <vladimir.gorea@gmail.com>
2021-03-29 18:21:56 +0700

    clean up deleted files

---
+++ app/app/settings/testing.py
@@ -0,0 +1,54 @@
+from . import *
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=env.str('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+if DEBUG:
+    raise ValueError('Running production settings with DEBUG set to True')
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+VERSATILEIMAGEFIELD_SETTINGS = {
+    'create_images_on_demand': False,
+    'jpeg_resize_quality': 80,
+}
+
+EMAIL_HOST = env.str('EMAIL_HOST')
+EMAIL_PORT = env.str('EMAIL_PORT')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.str('EMAIL_USE_TLS', False)
+EMAIL_USE_SSL = env.str('EMAIL_USE_SSL', False)
+SERVER_EMAIL = env.str('EMAIL_HOST_USER')
