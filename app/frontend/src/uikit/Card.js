76e170c0fdb4505a50e484f2dac4261cec7f434c
Elijah Pekson <etrpekson1998@gmail.com>
2021-09-04 23:09:53 +0800

    update card component to cater to cards with images and more detailed descriptions

--- app/frontend/src/uikit/Card.js
+++ app/frontend/src/uikit/Card.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { shortenText } from '../utils/strings';
 
 const Card = ({
     title,
     subtitle,
     description,
+    image,
+    subDescription,
     actions,
+    link,
+    isDetailedDescription = false,
 }) => {
+    const renderPlainDescription = () => {
+        return (
+            <div className="card-description">
+                <p>{shortenText(description, 0, 150)}...</p>
+            </div>
+        )
+    }
+
+    const renderDetailedDescription = () => {
+        const detailedDescription = (
+            <div className="detailed-description">
+                <h3>{shortenText(description, 0, 75)}</h3>
+                <p>
+                    {shortenText(subDescription, 0, 60)}...
+                    <a href={link}>View more</a>
+                </p>
+            </div>
+        );
+        if (image) {
+            return (
+                <div className="detailed-description-with-image">
+                    <img src={image} alt={description} />
+                    {detailedDescription}
+                </div>
+            )
+        }
+
+        return detailedDescription;
+    }
+
+    const renderDescription = isDetailedDescription ? renderDetailedDescription() : renderPlainDescription();
+
     return (
         <div className="card">
             <div className="card-content">
@@ -17,11 +54,9 @@ const Card = ({
                         </p>
                     }
                 </div>
-                <div className="card-description">
-                    <p>{description}</p>
-                </div>
+                {renderDescription}
             </div>
-            
+            <div />
             <div className="card-footer">
                 {actions}
             </div>
