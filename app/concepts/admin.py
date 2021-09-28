718fc38e54a11be5bc343b9515d29d96af948bc7
vladimir.gorea <vladimir.gorea@gmail.com>
2021-09-29 02:16:37 +0700

    Add resource history and concepts to admin

--- app/concepts/admin.py
+++ app/concepts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.CategoryConcept)
+class CategoryConceptAdmin(MPTTModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('name', 'author', 'description', 'created_at')
+
+
+@admin.register(models.TechnologyConcept)
+class TechnologyConceptAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('name', 'author', 'description', 'created_at')
--- app/history/admin.py
+++ app/history/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.ResourceHistoryModel)
+class ResourceHistoryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    list_display = ('created','content_object', 'author', 'edit_published_by', 'edit_reason', 'operation_source', 'operation_type')
