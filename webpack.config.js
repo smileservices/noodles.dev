369d1c98b59b783fa4fa0a08eb2c6b725969b8a5
vladimir.gorea <vladimir.gorea@gmail.com>
2022-01-03 12:04:52 +0700

    add app/static/css and js to .gitignore and reconfigure compiled js destinations

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = (env, argv) => ({
     externals: {
         'react': 'React',
         'react-dom': 'ReactDOM',
@@ -7,54 +7,53 @@ module.exports = {
         // just create the react-bundle once
 
         //for dev
-        'static/js/react-bundle': ['./app/static/js/react.development.js', './app/static/js/react-dom.development.js'],
-
-        //for prod
-        // 'static/js/react-bundle': ['./app/static/js/react.min.js', './app/static/js/react-dom.min.js'],
+        'static/js/react-bundle': argv.mode === 'production'
+            ? ['./app/static/react_files/react.min.js', './app/static/react_files/react-dom.min.js']
+            : ['./app/static/react_files/react.development.js', './app/static/react_files/react-dom.development.js'],
 
         //homepage
-        '/static/homepage-app': './app/frontend/src/HomepageApp.js',
+        '/static/js/homepage-app': './app/frontend/src/HomepageApp.js',
 
         //study resource related
-        '/static/study_resource/study-resource-detail-reviews': './app/study_resource/src/StudyResourceDetailReviews.js',
-        '/static/study_resource/study-resource-create': './app/study_resource/src/create/StudyResourceCreate.js',
-        '/static/study_resource/detail-toolbar': './app/study_resource/src/detail/ToolbarApp.js',
-        '/static/study_resource/study-resource-edit': './app/study_resource/src/EditApp.js',
-        '/static/my-study-resources': './app/study_resource/src/MyResourcesApp.js',
+        '/static/js/study_resource/study-resource-detail-reviews': './app/study_resource/src/StudyResourceDetailReviews.js',
+        '/static/js/study_resource/study-resource-create': './app/study_resource/src/create/StudyResourceCreate.js',
+        '/static/js/study_resource/detail-toolbar': './app/study_resource/src/detail/ToolbarApp.js',
+        '/static/js/study_resource/study-resource-edit': './app/study_resource/src/EditApp.js',
+        '/static/js/my-study-resources': './app/study_resource/src/MyResourcesApp.js',
 
         //study resource collections
-        '/static/my-study-collections': './app/study_collection/src/MyCollectionsApp.js',
+        '/static/js/my-study-collections': './app/study_collection/src/MyCollectionsApp.js',
 
         //technology
-        '/static/technology/technology-create': './app/technology/src/TechCreateApp.js',
-        '/static/technology/my-technologies-app': './app/technology/src/MyTechnologiesApp.js',
+        '/static/js/technology/technology-create': './app/technology/src/TechCreateApp.js',
+        '/static/js/technology/my-technologies-app': './app/technology/src/MyTechnologiesApp.js',
 
         //concept
-        '/static/concepts/category/edit': './app/concepts/src/category/EditApp.js',
-        '/static/concepts/category/create': './app/concepts/src/category/CreateApp.js',
-        '/static/concepts/technology/edit': './app/concepts/src/technology/EditApp.js',
-        '/static/concepts/technology/create': './app/concepts/src/technology/CreateApp.js',
+        '/static/js/concepts/category/edit': './app/concepts/src/category/EditApp.js',
+        '/static/js/concepts/category/create': './app/concepts/src/category/CreateApp.js',
+        '/static/js/concepts/technology/edit': './app/concepts/src/technology/EditApp.js',
+        '/static/js/concepts/technology/create': './app/concepts/src/technology/CreateApp.js',
 
         //vote app
-        '/static/votes': './app/src/core/VotesApp.js',
+        '/static/js/votes': './app/src/core/VotesApp.js',
 
         //search app
-        '/static/navbar-search-bar-app': './app/search/src/NavbarSearchBarApp.js',
-        '/static/search-app': './app/search/src/SearchApp.js',
-        '/static/minimal-search-app': './app/search/src/MinimalSearchApp.js',
+        '/static/js/navbar-search-bar-app': './app/search/src/NavbarSearchBarApp.js',
+        '/static/js/search-app': './app/search/src/SearchApp.js',
+        '/static/js/minimal-search-app': './app/search/src/MinimalSearchApp.js',
 
         //edit suggestions
-        '/static/technology/edit': './app/technology/src/EditApp.js',
-        '/static/study_resource/edit': './app/study_resource/src/EditApp.js',
+        '/static/js/technology/edit': './app/technology/src/EditApp.js',
+        '/static/js/study_resource/edit': './app/study_resource/src/EditApp.js',
 
         //sidebar
-        '/static/frontend/sidebar-app': './app/frontend/src/sidebar/SidebarApp.js',
+        '/static/js/frontend/sidebar-app': './app/frontend/src/sidebar/SidebarApp.js',
 
         //related section
-        '/static/related-app': './app/frontend/src/RelatedApp.js',
+        '/static/js/related-app': './app/frontend/src/RelatedApp.js',
 
         //users
-        '/static/profile-app': './app/users/src/ProfileApp.js',
+        '/static/js/profile-app': './app/users/src/ProfileApp.js',
 
         //history
         '/static/js/resource-history': './app/history/src/ResourceHistoryApp.js'
@@ -77,4 +76,4 @@ module.exports = {
             }
         ]
     }
-};
+});
