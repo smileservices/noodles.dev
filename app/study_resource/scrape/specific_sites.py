ff7be663394c52d1a6e0c3c6575b97bb1bd37d8d
Sosshi <symonmwenex@gmail.com>
2022-06-09 16:44:42 +0200

    added restructured freecodecamp class to process the two main content section differently

--- app/study_resource/scrape/specific_sites.py
+++ app/study_resource/scrape/specific_sites.py
@@ -2,6 +2,7 @@ import requests
 
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
+from gensim.summarization import summarize, keywords
 
 
 class Scrape:
@@ -44,6 +45,33 @@ class Youtube(Scrape):
 
 class FreeCodeCamp(Scrape):
     def process_data(self) -> dict:
+        """Some of the Tutorials on the site are from the news section and they have all
+            all the information needed like author, and date created while others are from the 
+            organization itself tutorial so they do not have authors"""
+        if "news" in self.url:
+            return self.process_tutorial_from_news_section()
+        return self.process_default_tutorial()
+        
+    def process_tutorial_from_news_section(self):
+        result = {}
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto(self.url)
+            result["title"] = page.title()
+            result["top_img"] = page.query_selector("picture").query_selector("img").get_attribute("src")
+            result["publishing_date"] = page.query_selector("time").inner_text()
+            result["created_by"] = page.query_selector(".author-card-name").query_selector("a").inner_text()
+            result["summary"] = page.query_selector_all("p")
+            paragraphs = page.query_selector_all("p")
+            paragraphs_list = [paragraph.inner_text() for paragraph in paragraphs]
+            result["summary"] = summarize(" ".join(paragraphs_list)).replace("\n", "")
+            result["tags"] = keywords(result["summary"], ratio=0.03, split=True, lemmatize=True)
+
+        return result
+
+    
+    def process_default_tutorial(self):
         result = {}
         free_code_camp_page_data = self._get_data()
         soup = BeautifulSoup(free_code_camp_page_data, "html.parser")
@@ -51,9 +79,10 @@ class FreeCodeCamp(Scrape):
         result["top_img"] = soup.find("meta", {"property": "og:image"})["content"]
         result["publishing_date"] = ""
         result["created_by"] = ["Freecodecamp"]
-        result["summary"] = self._get_summary()
+        result["summary"] = self._get_summary().replace("\n","")
+        result["tags"] = keywords(result["summary"], ratio=0.03, split=True, lemmatize=True)
 
-        return result
+        return result   
 
     def _get_summary(self) -> str:
         with sync_playwright() as p:
@@ -62,10 +91,10 @@ class FreeCodeCamp(Scrape):
             page = browser.new_page()
             page.goto(self.url)
             paragraphs = page.query_selector_all("p")
+            paragraphs_list = [paragraph.inner_text() for paragraph in paragraphs]
 
-            for count, paragraph in enumerate(paragraphs):
-                summary += paragraph.inner_text()
-                if count == 3:
-                    break
-            browser.close()
+            summary = summarize(" ".join(paragraphs_list))
             return summary
+
+a = FreeCodeCamp("https://www.freecodecamp.org/news/python-enumerate-what-is-the-enumerate-function-in-python/").process_data()
+print(a)
